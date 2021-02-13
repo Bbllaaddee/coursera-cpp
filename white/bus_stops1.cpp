f8bb545f7d01ea19b89305a783eb7ee415c523bf
Bbllaaddee <mr.kamorzin@yandex.ru>
2021-02-14 00:27:46 +0300

    nothing

---
+++ white/bus_stops1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main()
+{
+	std::map<std::string, std:vector<std::string>> bs;
+	int n;
+	std::cin >> n;
+	for(int i=0; i<n; ++i)
+	{
+		std::string query;
+		std::cin >> query;
+		if(query=="NEW_BUS")
+		{
+			std::string bus;
+			int n_stops;
+			std::cin >> bus >> n_stops;
+			for()
+		}
+	}
+}
